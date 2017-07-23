36c2ad61f063eba066186c2667c56ec333273fc6
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-07-23 12:05:23 +0530

    cmen/google chart setup successfuly

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace AppBundle\Controller;
-
-use AppBundle\ChartsAndGraphs\Drawer;
+use CMEN\GoogleChartsBundle\GoogleCharts\Charts\PieChart;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,17 +13,25 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $pieChart = new PieChart();
+        $pieChart->getData()->setArrayToDataTable(
+            [['Task', 'Hours per Day'],
+                ['Work',     11],
+                ['Eat',      2],
+                ['Commute',  2],
+                ['Watch TV', 2],
+                ['Sleep',    7]
+            ]
+        );
+        $pieChart->getOptions()->setTitle('My Daily Activities');
+        $pieChart->getOptions()->setHeight(500);
+        $pieChart->getOptions()->setWidth(900);
+        $pieChart->getOptions()->getTitleTextStyle()->setBold(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setColor('#009900');
+        $pieChart->getOptions()->getTitleTextStyle()->setItalic(true);
+        $pieChart->getOptions()->getTitleTextStyle()->setFontName('Arial');
+        $pieChart->getOptions()->getTitleTextStyle()->setFontSize(20);
 
-        $drawer = new Drawer();
-        $drawer->columnChart();
-//        return $this->render('default/index.html.twig', [
-//            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-//        ]);
-
-        return $this->render('dashboard/home.html.twig', array(
-            'chart' => $drawer,
-        ));
-
+        return $this->render('dashboard/index.html.twig', array('piechart' => $pieChart));
     }
 }
